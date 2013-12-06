5c6b1a0958723477e403fabaee3b3660d6ef68f5
ShadauxCat <shadowcat.github@phasethrough.com>
2013-12-06 13:40:24 -0600

    Added asynchronous network module, previous network module is now deprecated

---
+++ network/src/async_network.hpp
@@ -0,0 +1,336 @@
+#pragma once
+
+/*
+ * This module is included as a part of libSprawl
+ *
+ * Copyright (C) 2013 Jaedyn K. Draper
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+//Fix bug in llvm...
+#ifndef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_1
+#define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_1
+#endif
+
+#ifndef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_2
+#define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_2
+#endif
+
+#ifndef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_4
+#define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_4
+#endif
+
+#ifndef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_8
+#define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_8
+#endif
+
+//C++ includes
+#include <string>
+#include <thread>
+#include <mutex>
+#include <sstream>
+#include <vector>
+#include <memory>
+#include <map>
+#include <unordered_set>
+
+//C includes
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <sys/time.h>
+
+////////////////////////////////////////////////////////////////////////////////
+/// TODO:
+/// - Machine ID and Process ID in UDP connections
+/// - network byte order on UDP headers
+/// - Move function implementations into cpp file
+/// - Split up UDP packets that are larger than MTU
+/// - Combine UDP packets smaller than MTU
+/// - Ordered UDP implementation
+/// - Change implementation from select() to epoll()
+/// - Windows compatibility
+/// - Server socket store connections as a map so we know which connection to Recv() on in UDP
+/// - Handle high_id overflow
+/// - UDP connection timeout configurable
+/// - Remove usages of std::string
+////////////////////////////////////////////////////////////////////////////////
+
+namespace sprawl
+{
+	namespace async_network
+	{
+		typedef std::function<void(const std::shared_ptr<class Connection>, const std::string&)> ReceiveCallback;
+		typedef std::function<void(const std::shared_ptr<class Connection>)> ConnectionCallback;
+		typedef std::function<void(void)> SendCallback;
+		typedef std::function<bool(std::string&, std::string&)> PacketValidationCallback;
+
+		class SockExcept: public std::exception
+		{
+		public:
+			SockExcept(const std::string& arg)
+				: m_str(arg)
+			{
+			}
+			~SockExcept() throw ()
+			{
+			}
+			const char* what() const throw ()
+			{
+				return m_str.c_str();
+			}
+		private:
+			std::string m_str;
+		};
+
+		enum class FailType { resend, ignore, notify };
+		enum class ConnectionType { TCP, UDP };
+
+		//Simple connection information.
+		class Connection : public std::enable_shared_from_this<Connection>
+		{
+		public:
+			//Information about the connection
+			int GetDescriptor();
+			std::string GetHostname();
+			int GetPort();
+
+			//Send is asynchronous - data is passed in on the main thread and then sent on the network thread
+			virtual void Send(const std::string& data, SendCallback onSendFunction = nullptr);
+
+			virtual void Send(const std::string& str, FailType /*behavior*/, SendCallback callback = nullptr)
+			{
+				Send(str, callback);
+			}
+
+		protected:
+			friend class ServerSocket;
+			friend class ClientSocket;
+
+			Connection(int desc_, struct sockaddr* addr, ReceiveCallback onReceive_, PacketValidationCallback validatePacket_);
+
+			//This send is called by the socket and actually sends everything collected by the other send
+			//Will call the onSend callback when it finishes
+			virtual void Send();
+			//Receive data, will call the onReceived callback if there's anything here
+			virtual int Recv();
+
+			int m_desc;
+			struct sockaddr m_dest;
+
+			bool m_closeSocket;
+			std::string m_partialPacket;
+
+			ReceiveCallback m_onReceive;
+			PacketValidationCallback m_validatePacket;
+
+			std::vector< std::pair<std::string, SendCallback> > m_outData;
+			std::mutex m_outDataMutex;
+		};
+
+		class UDPConnection : public Connection
+		{
+		public:
+			virtual void Send(const std::string& str, SendCallback callback = nullptr) override final
+			{
+				Send(str, FailType::ignore, callback);
+			}
+			virtual void Send(const std::string& str, FailType behavior, SendCallback callback = nullptr) override final;
+		protected:
+			friend class ServerSocket;
+			friend class ClientSocket;
+
+			UDPConnection(int desc_, struct sockaddr* addr, ReceiveCallback onReceive_, PacketValidationCallback validatePacket_);
+
+			UDPConnection(int desc_, ReceiveCallback onReceive_, PacketValidationCallback validatePacket_);
+
+			virtual void Send() override final;
+			virtual int Recv() override final;
+			bool CheckClosed();
+			void SendKeepAlive();
+		private:
+			struct packet
+			{
+				packet(uint32_t _id, FailType _behavior, const std::string& _content, const std::string& header_);
+				uint32_t m_ID;
+				struct timeval m_sentTime;
+				FailType m_behavior;
+				const std::string m_content;
+				const std::string m_header;
+			};
+
+			std::vector< std::pair<packet, SendCallback> > m_outPackets;
+			std::map<int, packet> m_packets;
+			std::unordered_set<int> m_received;
+			int m_highId;
+			uint32_t m_currentId;
+			struct sockaddr m_src;
+			struct timeval m_lastRcvd;
+			struct timeval m_lastSent;
+			socklen_t m_slen;
+
+			void SendPacketWithID(const std::string& str, FailType behavior, int32_t sendid, SendCallback callback);
+		};
+
+		//Asynchronous socket class
+		class ServerSocket
+		{
+		public:
+			ServerSocket(const ConnectionType connectionType);
+
+			//Set callbacks
+			void SetOnReceive( ReceiveCallback c );
+
+			void SetOnConnect( ConnectionCallback c );
+
+			void SetOnClose( ConnectionCallback c );
+
+			void SetPacketValidator( PacketValidationCallback c );
+
+			//listen() not only opens the port, but actually starts a network thread to handle connections on it
+			void listen(int port);
+
+			//Close() stops the network thread and closes the port
+			void Close();
+
+			~ServerSocket();
+
+			//Set timeout in microseconds.
+			void setTimeout(int timeout);
+
+			//Accessors to get and close connections.
+			//Connections are always returned as weak pointers, if a reference is held by client code it may become invalid later.
+			//This ensures the client never has a corrupt pointer to a connection that's been closed
+			std::vector< std::weak_ptr<Connection> > GetConnections();
+
+			std::weak_ptr<Connection> GetConnection(int i);
+
+			std::weak_ptr<Connection> GetConnectionByDesc(int d);
+
+			std::weak_ptr<Connection> GetConnectionByPort(int p);
+
+			int GetNumConnections();
+
+			void CloseConnection(int i);
+
+			void CloseConnection(std::shared_ptr<Connection> c);
+
+		private:
+			//Starts the network thread
+			void RunThread();
+			//Runs general IO
+			void HandleIO();
+
+
+		private:
+			int m_inSock;
+			int m_inPort;
+			struct timeval m_tv;
+
+			ConnectionCallback m_onConnect;
+			ConnectionCallback m_onClose;
+			ReceiveCallback m_onReceive;
+			PacketValidationCallback m_packetValidator;
+
+			bool m_running;
+
+			std::vector< std::shared_ptr<Connection> > m_connections;
+
+			fd_set m_inSet;
+			fd_set m_excSet;
+			struct addrinfo m_hints;
+			struct addrinfo* m_servInfo;
+			std::thread m_thread;
+			std::mutex m_mtx;
+
+			ConnectionType m_connectionType;
+		};
+
+		class ClientSocket
+		{
+		public:
+			ClientSocket(ConnectionType connectionType);
+
+			~ClientSocket();
+
+			//Set timeout in microseconds.
+			void setTimeout(int timeout);
+
+			void Connect(const std::string& addr, int port);
+
+			void Reconnect();
+
+			//Set callbacks
+			void SetOnReceive( ReceiveCallback c );
+
+			void SetOnConnect( ConnectionCallback c );
+
+			void SetOnClose( ConnectionCallback c );
+
+			void SetPacketValidator( PacketValidationCallback c );
+
+			std::weak_ptr<Connection> GetConnection()
+			{
+				return m_con;
+			}
+
+			void Send(const std::string& str)
+			{
+				m_con->Send(str);
+			}
+
+			void Send(const std::string& str, FailType behavior)
+			{
+				m_con->Send(str, behavior);
+			}
+
+			void Close();
+
+		private:
+			//Starts the network thread
+			void RunThread();
+
+			void HandleIO();
+
+			ConnectionCallback m_onConnect;
+			ConnectionCallback m_onClose;
+			ReceiveCallback m_onReceive;
+			PacketValidationCallback m_packetValidator;
+
+			std::shared_ptr<Connection> m_con;
+			int m_sock;
+			fd_set m_inSet;
+			fd_set m_excSet;
+			struct addrinfo m_hints;
+			struct addrinfo* m_servInfo;
+			struct timeval m_tv;
+
+			bool m_running;
+			std::thread m_thread;
+
+			ConnectionType m_connectionType;
+		};
+	}
+}
