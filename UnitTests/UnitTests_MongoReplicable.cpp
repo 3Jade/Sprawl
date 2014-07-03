51c94b1b377ad04489c9f8aa4b55ace62ffd7217
ShadauxCat <shadowcat.github@phasethrough.com>
2014-07-03 08:02:57 -0500

    Fixed an incorrect assert in mongo replicable unit test that was based on a previous bug in the code.

--- UnitTests/UnitTests_MongoReplicable.cpp
+++ UnitTests/UnitTests_MongoReplicable.cpp
@@ -55,20 +55,29 @@ bool test_mongo_replicable()
 
 	std::vector<mongo::BSONObj> deltas = m.generateUpdateQuery();
 
-#define ASSERT(condition, message) if(!(!(condition))==false){ success = false; printf("%s\n... ", message); }
+#define ASSERT(condition, message, ...) if(!(!(condition))==false){ success = false; printf(message, ## __VA_ARGS__); printf("\n..."); }
 
 	ASSERT(deltas.size() == 3, "Test case should have returned three queries.");
 	ASSERT(
 		deltas[0].jsonString(mongo::TenGen) ==
-			R"raw({ "$set" : { "t" : { "vect" : [ { "vect" : [] } ] } }, "$unset" : { "t" : "", "t.vect" : "", "t.vect.0" : "", "t.vect.0.vect" : "", "t.vect.0.vect.2" : "", "t.vect.1" : "" } })raw",
-			"Test case returned invalid first query");
+			R"raw({ "$unset" : { "t.vect.0.vect.2" : "", "t.vect.1" : "" } })raw",
+			"Test case returned invalid first query. Should have been:\n\n\t"
+			R"raw({ "$unset" : { "t.vect.0.vect.2" : "", "t.vect.1" : "" } })raw"
+			"\n\nGot:\n\n\t%s",
+			deltas[0].jsonString(mongo::TenGen).c_str());
 	ASSERT(
 		deltas[1].jsonString(mongo::TenGen) ==
 			R"raw({ "$pull" : { "t.vect.0.vect" : null } })raw",
-			"Test case returned invalid second query");
+			"Test case returned invalid second query Should have been:\n\n\t"
+			R"raw({ "$pull" : { "t.vect.0.vect" : null } })raw"
+			"\n\nGot:\n\n\t%s",
+			deltas[1].jsonString(mongo::TenGen).c_str());
 	ASSERT(
 		deltas[2].jsonString(mongo::TenGen) ==
 			R"raw({ "$pull" : { "t.vect" : null } })raw",
-			"Test case returned invalid third query");
+			"Test case returned invalid third query Should have been:\n\n\t"
+			R"raw({ "$pull" : { "t.vect" : null } })raw"
+			"\n\nGot:\n\n\t%s",
+			deltas[2].jsonString(mongo::TenGen).c_str());
 	return success;
 }
